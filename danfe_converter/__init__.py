b5a36e31171396b45b73199aee2dbddd33776e1f
ogabrieltech <ogabrieltech@gmail.com>
2026-09-08 10:06:02 -0300

    refactor: expose public package API

---
+++ danfe_converter/__init__.py
@@ -0,0 +1,16 @@
+from .core import (
+    APP_NAME,
+    APP_VERSION,
+    BatchResult,
+    InvoiceInfo,
+    XmlDocument,
+    collect_documents,
+    generate_batch,
+    parse_invoice,
+    safe_filename,
+)
+
+__all__ = [
+    "APP_NAME", "APP_VERSION", "BatchResult", "InvoiceInfo", "XmlDocument",
+    "collect_documents", "generate_batch", "parse_invoice", "safe_filename",
+]
